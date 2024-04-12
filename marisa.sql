86b55d8ec77041679659dc9655245783c75ef06a
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-04-12 18:31:35 -0300

    Create marisa.sql

---
+++ marisa.sql
@@ -0,0 +1,23 @@
+create database marisa;
+use marisa;
+create table clientes(
+id int auto_increment primary key,
+nome varchar(20) not null,
+cidade varchar(40)
+);
+create table pedidos(
+id int auto_increment primary key,
+produto varchar (50) not null,
+quantidade int not null,
+clientes_id int,
+foreign key(clientes_id)references clientes(id)
+
+);
+drop table pedidos;
+drop table clientes;
+drop database marisa;
+
+SELECT c.nome
+FROM clientes c
+INNER JOIN pedidos p ON c.id= p.clientes_id
+WHERE p.produto = &#39;camiseta&#39;;
